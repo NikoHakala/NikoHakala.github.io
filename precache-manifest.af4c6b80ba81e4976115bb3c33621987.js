a6dd3a77f8aaa6b3886c215049d793b4cc8adefb
NikoHakala <42651666+NikoHakala@users.noreply.github.com>
2019-11-01 18:15:50 +0200

    Test build for react tinyforest

---
+++ precache-manifest.af4c6b80ba81e4976115bb3c33621987.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae400eb6c000bb50ba2e91d534b17c8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85b34e888a8c42c94b06",
+    "url": "/static/css/2.3f777cba.chunk.css"
+  },
+  {
+    "revision": "d02c4eef3d08919bf001",
+    "url": "/static/css/main.553ef49c.chunk.css"
+  },
+  {
+    "revision": "85b34e888a8c42c94b06",
+    "url": "/static/js/2.af864e25.chunk.js"
+  },
+  {
+    "revision": "d02c4eef3d08919bf001",
+    "url": "/static/js/main.07601a8f.chunk.js"
+  },
+  {
+    "revision": "6d362d24b711615405e0",
+    "url": "/static/js/runtime-main.a24a4ec9.js"
+  }
+]);
